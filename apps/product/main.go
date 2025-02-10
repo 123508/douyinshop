81f0ca60f6fd815b83b7e4eb1a69d8212fc7cc8f
123508 <47135770+123508@users.noreply.github.com>
2025-02-10 12:11:46 +0800

    feat(pkg/config): 添加ElasticSearch配置 (#9)

--- apps/product/main.go
+++ apps/product/main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
+	"fmt"
+	"github.com/123508/douyinshop/pkg/config"
 	"log"
 
 	product "github.com/123508/douyinshop/kitex_gen/product/productcatalogservice"
+	"github.com/elastic/go-elasticsearch/v8"
 )
 
 func main() {
+	cfg := elasticsearch.Config{
+		Addresses: config.Conf.ElasticSearch.Hosts,
+		Username:  config.Conf.ElasticSearch.Username,
+		Password:  config.Conf.ElasticSearch.Password,
+	}
+	els, err := elasticsearch.NewClient(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	// 测试代码
+	x, err := els.Get("product", "1")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(x)
+
 	svr := product.NewServer(new(ProductCatalogServiceImpl))
 
-	err := svr.Run()
+	err = svr.Run()
 
 	if err != nil {
 		log.Println(err.Error())
