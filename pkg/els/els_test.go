82c4471b8dd5e61d7a373e98e2ccea4e4833bc1d
123508 <973699883@qq.com>
2025-02-19 15:26:26 +0800

    feat(pkg/els, apps/product): els搜索商品函数添加page和size参数, 商品服务实现搜索商品功能

--- pkg/els/els_test.go
+++ pkg/els/els_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestSearchProduct(t *testing.T) {
 	name := "手机"
-	got, err := SearchProduct(name)
+	got, err := SearchProduct(name, 1, 2)
 	if err != nil {
 		t.Errorf("SearchProduct() error = %v", err)
 		return
