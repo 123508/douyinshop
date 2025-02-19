2c48d534838c6d4ba501b710c7b42f248df06074
wxr <1565858376@qq.com>
2025-02-19 21:10:34 +0800

    checkout服务代码，还有一些小细节，比如参数的传递没有弄清楚，后续完善

--- apps/checkout/handler.go
+++ apps/checkout/handler.go
@@ -2,15 +2,136 @@ package main
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"github.com/123508/douyinshop/kitex_gen/cart"
+	"github.com/123508/douyinshop/kitex_gen/cart/cartservice"
+	_ "github.com/123508/douyinshop/kitex_gen/cart/cartservice"
+	_ "github.com/123508/douyinshop/kitex_gen/order/order_common"
+	"github.com/123508/douyinshop/kitex_gen/order/userOrder"
+	"github.com/123508/douyinshop/kitex_gen/order/userOrder/orderuserservice"
+	"github.com/123508/douyinshop/kitex_gen/payment"
+	"github.com/123508/douyinshop/kitex_gen/payment/paymentservice"
+	"github.com/123508/douyinshop/kitex_gen/product"
+	"github.com/123508/douyinshop/kitex_gen/product/productcatalogservice"
+	"github.com/cloudwego/kitex/client"
+	"github.com/cloudwego/kitex/pkg/klog"
+	_ "go.opentelemetry.io/otel"
+	_ "go.opentelemetry.io/otel/propagation"
+	"strconv"
+	"sync"
 
 	checkout "github.com/123508/douyinshop/kitex_gen/checkout"
 )
 
 // CheckoutServiceImpl implements the last service interface defined in the IDL.
-type CheckoutServiceImpl struct{}
+type CheckoutServiceImpl struct {
+	ctx context.Context
+}
+
+var (
+	CartClient    cartservice.Client
+	ProductClient productcatalogservice.Client
+	PaymentClient paymentservice.Client
+	OrderClient   orderuserservice.Client
+	once          sync.Once
+	err           error
+	registryAddr  string
+	serviceName   string
+	commonSuite   client.Option
+)
 
 // Checkout implements the CheckoutServiceImpl interface.
 func (s *CheckoutServiceImpl) Checkout(ctx context.Context, req *checkout.CheckoutReq) (resp *checkout.CheckoutResp, err error) {
 	// TODO: Your code here...
+	//get cart
+	cartResult, err := CartClient.GetCart(s.ctx, &cart.GetCartReq{UserId: req.UserId})
+
+	if err != nil {
+		klog.Error(err)
+		err = fmt.Errorf("GetCart.err:%v", err)
+		return
+	}
+	if cartResult == nil || cartResult.Cart == nil || len(cartResult.Cart.Items) == 0 {
+		err = errors.New("cart is empty")
+		return
+	}
+	var (
+		//oi    []*order_common.Order
+		total float32
+	)
+	for _, cartItem := range cartResult.Cart.Items {
+		productResp, resultErr := ProductClient.GetProduct(s.ctx, &product.GetProductReq{Id: cartItem.ProductId})
+		if resultErr != nil {
+			klog.Error(resultErr)
+			err = resultErr
+			return
+		}
+		if productResp.Product == nil {
+			continue
+		}
+		p := productResp.Product
+		cost := p.Price * float32(cartItem.Quantity)
+		total += cost
+		/*oi = append(oi, &order_common.Order{
+		}*/
+	}
+	//create order
+	orderReq := &userOrder.OrderSubmitReq{
+		UserId:    req.UserId,
+		PayMethod: 1, //不知道如何传入
+		Remark:    "0",
+		Amount:    total,
+	}
+
+	if req.Address != nil {
+		orderReq.AddressBookId = 1 //不知道如何传入
+	}
+	orderResult, err := OrderClient.Submit(s.ctx, orderReq)
+	if err != nil {
+		err = fmt.Errorf("Submit.err:%v", err)
+		return
+	}
+	klog.Info("orderResult", orderResult)
+	// empty cart
+	emptyResult, err := CartClient.EmptyCart(s.ctx, &cart.EmptyCartReq{UserId: req.UserId})
+	if err != nil {
+		err = fmt.Errorf("EmptyCart.err:%v", err)
+		return
+	}
+	klog.Info(emptyResult)
+	// charge
+	var orderId uint32
+	if orderResult != nil || orderResult.OrderId != 0 {
+		orderId = orderResult.OrderId
+	}
+	payReq := &payment.ChargeReq{
+		UserId:  req.UserId,
+		OrderId: strconv.Itoa(int(orderId)),
+		Amount:  total,
+		CreditCard: &payment.CreditCardInfo{
+			CreditCardNumber:          req.CreditCard.CreditCardNumber,
+			CreditCardExpirationYear:  req.CreditCard.CreditCardExpirationYear,
+			CreditCardExpirationMonth: req.CreditCard.CreditCardExpirationMonth,
+			CreditCardCvv:             req.CreditCard.CreditCardCvv,
+		},
+	}
+	paymentResult, err := PaymentClient.Charge(s.ctx, payReq)
+	if err != nil {
+		err = fmt.Errorf("Charge.err:%v", err)
+		return
+	}
+
+	// otel inject
+
+	klog.Info(paymentResult)
+	// change order state
+	klog.Info(orderResult)
+
+	resp = &checkout.CheckoutResp{
+		OrderId:       strconv.Itoa(int(orderId)),
+		TransactionId: paymentResult.TransactionId,
+	}
 	return
+
 }
--- apps/checkout/main.go
+++ apps/checkout/main.go
@@ -1,15 +1,46 @@
 package main
 
 import (
+	"fmt"
+	"github.com/123508/douyinshop/pkg/config"
+	"github.com/123508/douyinshop/pkg/db"
+	"github.com/123508/douyinshop/pkg/models"
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
+	"github.com/cloudwego/kitex/server"
+	etcd "github.com/kitex-contrib/registry-etcd"
 	"log"
+	"net"
 
 	checkout "github.com/123508/douyinshop/kitex_gen/checkout/checkoutservice"
 )
 
 func main() {
-	svr := checkout.NewServer(new(CheckoutServiceImpl))
+	database, err := db.InitDB()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	err := svr.Run()
+	database.AutoMigrate(&models.AddressBook{})
+
+	r, err := etcd.NewEtcdRegistryWithAuth(config.Conf.EtcdConfig.Endpoints, config.Conf.EtcdConfig.Username, config.Conf.EtcdConfig.Password)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	addr, _ := net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:%d", config.Conf.CheckoutConfig.Host, config.Conf.CheckoutConfig.Port))
+
+	svr := checkout.NewServer(
+		new(CheckoutServiceImpl),
+		server.WithServiceAddr(addr),
+		server.WithRegistry(r),
+		server.WithServerBasicInfo(
+			&rpcinfo.EndpointBasicInfo{
+				ServiceName: config.Conf.AddressConfig.ServiceName,
+			},
+		),
+	)
+
+	err = svr.Run()
 
 	if err != nil {
 		log.Println(err.Error())
