1e59f587e2073d417a02a44edaba43760ed4f53e
123508 <47135770+123508@users.noreply.github.com>
2025-02-20 20:04:20 +0800

    feat(pkg/models): 更新order表和order detail表，添加了order status log表 (#37)

---
+++ pkg/models/order_status_log.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type OrderStatusLog struct {
+	gorm.Model
+	OrderDetailId int       `gorm:"comment '订单详情id'"`
+	Status        uint      `gorm:"comment '订单状态 0待付款 1待接单 2已接单 3运输中 4待收货 5已完成 6已取消 7退款中 8已退款 9商家拒单'"`
+	StartTime     time.Time `gorm:"comment '状态开始时间'"`
+	EndTime       time.Time `gorm:"comment '状态结束时间'"`
+	Description   string    `gorm:"comment '状态描述'"`
+}
