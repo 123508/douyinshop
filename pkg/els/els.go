b0602a79cd202a14ddf8c693fb1a18e1bf7f3cd2
123508 <47135770+123508@users.noreply.github.com>
2025-02-20 14:30:55 +0800

    feat(pkg/els): 添加更新和删除ElasticSearch中商品信息的功能 (#33)
    * feat(pkg/els): 添加更新ElasticSearch中商品信息的功能
    
    * feat(pkg/els): 添加删除ElasticSearch中商品信息的功能

--- pkg/els/els.go
+++ pkg/els/els.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/123508/douyinshop/kitex_gen/product"
 	"github.com/123508/douyinshop/pkg/config"
 	"log"
 	"strconv"
@@ -98,4 +99,95 @@ func SearchProduct(name string, page int, size int) ([]uint32, error) {
 		result = append(result, uint32(id))
 	}
 	return result, nil
+}
+
+// UpdateProduct 更新ElasticSearch中的商品信息
+// product: 商品信息
+// 当商品不存在时，创建新商品
+// 当商品存在时，更新商品信息
+// 返回值: 错误信息
+func UpdateProduct(product *product.Product) error {
+	if es == nil {
+		return fmt.Errorf("ElasticSearch client is nil")
+	}
+	var buf bytes.Buffer
+	doc := map[string]interface{}{
+		"name":        product.Name,
+		"description": product.Description,
+		"price":       product.Price,
+		"categories":  product.Categories,
+		"sales":       product.Sales,
+	}
+
+	// 检查文档是否存在
+	res, err := es.Get("product", strconv.Itoa(int(product.Id)))
+	if err != nil {
+		return fmt.Errorf("error checking document existence: %s", err)
+	}
+	if res.IsError() {
+		// 文档不存在，创建新文档
+		if err := json.NewEncoder(&buf).Encode(doc); err != nil {
+			return fmt.Errorf("error encoding doc: %s", err)
+		}
+		res, err := es.Index(
+			"product",
+			&buf,
+			es.Index.WithDocumentID(strconv.Itoa(int(product.Id))),
+		)
+		if err != nil {
+			return fmt.Errorf("error indexing: %s", err)
+		}
+		if res.IsError() {
+			var e map[string]interface{}
+			if err := json.NewDecoder(res.Body).Decode(&e); err != nil {
+				return fmt.Errorf("error parsing the response body: %s", err)
+			}
+			return fmt.Errorf("error: %s", e)
+		}
+	} else {
+		doc = map[string]interface{}{
+			"doc": doc,
+		}
+		// 文档存在，更新文档
+		if err := json.NewEncoder(&buf).Encode(doc); err != nil {
+			return fmt.Errorf("error encoding doc: %s", err)
+		}
+		res, err = es.Update(
+			"product",
+			strconv.Itoa(int(product.Id)),
+			&buf,
+		)
+		if err != nil {
+			return fmt.Errorf("error updating: %s", err)
+		}
+		if res.IsError() {
+			var e map[string]interface{}
+			if err := json.NewDecoder(res.Body).Decode(&e); err != nil {
+				return fmt.Errorf("error parsing the response body: %s", err)
+			}
+			return fmt.Errorf("error: %s", e)
+		}
+	}
+	return nil
+}
+
+// DeleteProduct 删除ElasticSearch中的商品信息
+// id: 商品id
+// 返回值: 错误信息
+func DeleteProduct(id uint32) error {
+	if es == nil {
+		return fmt.Errorf("ElasticSearch client is nil")
+	}
+	res, err := es.Delete("product", strconv.Itoa(int(id)))
+	if err != nil {
+		return fmt.Errorf("error deleting: %s", err)
+	}
+	if res.IsError() {
+		var e map[string]interface{}
+		if err := json.NewDecoder(res.Body).Decode(&e); err != nil {
+			return fmt.Errorf("error parsing the response body: %s", err)
+		}
+		return fmt.Errorf("error: %s", e)
+	}
+	return nil
 }
