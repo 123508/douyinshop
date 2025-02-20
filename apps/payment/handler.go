a7a97af1dc619e3db4e5b1a47803fd154ad93e9f
shiyuyu <2979724204@qq.com>
2025-02-20 16:59:44 +0800

    feat(apps/payment): 完成了Charge函数，写了etcd注册服务

--- apps/payment/handler.go
+++ apps/payment/handler.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"context"
-
+	"fmt"
 	payment "github.com/123508/douyinshop/kitex_gen/payment"
+	"github.com/123508/douyinshop/pkg/config"
+	"github.com/smartwalle/alipay/v3"
+	"log"
+	"math/rand"
+	"sync/atomic"
+	"time"
 )
 
 // PaymentServiceImpl implements the last service interface defined in the IDL.
@@ -11,12 +17,49 @@ type PaymentServiceImpl struct{}
 
 // Charge implements the PaymentServiceImpl interface.
 func (s *PaymentServiceImpl) Charge(ctx context.Context, req *payment.ChargeReq) (resp *payment.ChargeResp, err error) {
-	// TODO: Your code here...
-	return
+	if req.PayMethod == 2 {
+		// 初始化支付宝
+		var client *alipay.Client
+		if client, err = alipay.New(config.Conf.PaymentConfig.Alipay.AppId, config.Conf.PaymentConfig.Alipay.PrivateKey,
+			false); err != nil {
+			log.Fatal(err)
+		}
+		// 支付链接参数
+		var p = alipay.TradePagePay{}
+		p.Subject = "抖音商城购物"
+		p.OutTradeNo = req.OrderId
+		p.TotalAmount = fmt.Sprintf("%.2f", req.Amount)
+		p.ProductCode = "FAST_INSTANT_TRADE_PAY"
+		p.ReturnURL = ""
+		p.NotifyURL = ""
+		// 生成支付链接
+		orderUrl, _ := client.TradePagePay(p)
+
+		return &payment.ChargeResp{
+			TransactionId: "",
+			PayUrl:        orderUrl.String(),
+		}, nil
+	} else {
+		// 获取当前日期并格式化为 YYYYMMDD
+		currentDay := time.Now().Format("20060102")
+		// 生成一个12位的随机数
+		var counter uint64
+		count := atomic.AddUint64(&counter, 1)
+		countStr := fmt.Sprintf("%06d", count%1000000)         // 取计数器的后6位
+		randomPart1 := fmt.Sprintf("%06d", rand.Intn(1000000)) // 生成6位随机数
+		// 生成一个8位的随机数
+		randomPart2 := fmt.Sprintf("%08d", rand.Intn(100000000))
+		// 拼接成28位的订单号
+		tradeNo := currentDay + countStr + randomPart1 + randomPart2
+
+		return &payment.ChargeResp{
+			TransactionId: tradeNo,
+			PayUrl:        "",
+		}, nil
+	}
 }
 
 // Notify implements the PaymentServiceImpl interface.
 func (s *PaymentServiceImpl) Notify(ctx context.Context, req *payment.NotifyReq) (resp *payment.NotifyResp, err error) {
-	// TODO: Your code here...
 	return
 }
