c40a2fe7929b9fb7c7cf23fda338fdc109871a38
123508 <973699883@qq.com>
2025-02-21 15:07:35 +0800

    feat(apps/api): 添加初始化 AI, shop, cart, product, address, payment, checkout, order shop, order user 服务的kitex客户端初始化代码

---
+++ apps/api/infras/client/ai.go
@@ -0,0 +1,34 @@
+package client
+
+import (
+	"github.com/123508/douyinshop/kitex_gen/ai/aiservice"
+
+	"github.com/123508/douyinshop/pkg/config"
+
+	"time"
+
+	"github.com/cloudwego/kitex/client"
+	"github.com/cloudwego/kitex/pkg/retry"
+	etcd "github.com/kitex-contrib/registry-etcd"
+)
+
+var aiClient aiservice.Client
+
+func initAiRpc() {
+	r, err := etcd.NewEtcdResolverWithAuth(config.Conf.EtcdConfig.Endpoints, config.Conf.EtcdConfig.Username, config.Conf.EtcdConfig.Password)
+	if err != nil {
+		panic(err)
+	}
+
+	c, err := aiservice.NewClient(
+		config.Conf.AIConfig.ServiceName,                  // service name
+		client.WithRPCTimeout(3*time.Second),              // rpc timeout
+		client.WithConnectTimeout(50*time.Millisecond),    // conn timeout
+		client.WithFailureRetry(retry.NewFailurePolicy()), // retry
+		client.WithResolver(r),                            // resolver
+	)
+	if err != nil {
+		panic(err)
+	}
+	aiClient = c
+}
