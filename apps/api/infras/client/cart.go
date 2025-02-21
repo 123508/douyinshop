9744a8a161317115b0bb1bce121c77f595aaf050
chenguangyu <3069607974@qq.com>
2025-02-21 17:40:14 +0800

    feat(apps/api/infras/client/cart):完成添加、获取、删除、清空功能

--- apps/api/infras/client/cart.go
+++ apps/api/infras/client/cart.go
@@ -1,6 +1,8 @@
 package client
 
 import (
+	"context"
+	"github.com/123508/douyinshop/kitex_gen/cart"
 	"github.com/123508/douyinshop/kitex_gen/cart/cartservice"
 
 	"github.com/123508/douyinshop/pkg/config"
@@ -31,4 +33,40 @@ func initCartRpc() {
 		panic(err)
 	}
 	cartClient = c
+}
+
+// AddItem 添加商品到购物车
+func AddItem(ctx context.Context, req *cart.AddItemReq) (*cart.AddItemResp, error) {
+	resp, err := cartClient.AddItem(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+// GetCart获取购物车内容
+func GetCart(ctx context.Context, req *cart.GetCartReq) (*cart.GetCartResp, error) {
+	resp, err := cartClient.GetCart(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+// EmptyCart 清空购物车
+func EmptyCart(ctx context.Context, req *cart.EmptyCartReq) (*cart.EmptyCartResp, error) {
+	resp, err := cartClient.EmptyCart(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+// DeleteItem 删除购物车商品
+func DeleteItem(ctx context.Context, req *cart.DeleteItemReq) (*cart.EmptyCartResp, error) {
+	resp, err := cartClient.DeleteItem(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
 }
