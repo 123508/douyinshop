fc073be75c4d724f78ea74bf6995a14e92f69687
123508 <973699883@qq.com>
2025-02-23 20:18:11 +0800

    feat(apps/api/product): 完成获取商品列表、商品详情和搜索商品功能

--- apps/api/handlers/product/product_list.go
+++ apps/api/handlers/product/product_list.go
@@ -2,9 +2,39 @@ package product
 
 import (
 	"context"
+	"github.com/123508/douyinshop/apps/api/infras/client"
+	"strconv"
 
 	"github.com/cloudwego/hertz/pkg/app"
 )
 
+// List 获取商品列表
 func List(ctx context.Context, c *app.RequestContext) {
+	page, err := strconv.Atoi(c.Query("page"))
+	if err != nil {
+		c.JSON(400, map[string]interface{}{
+			"error": "page参数错误",
+		})
+		return
+	}
+	pageSize, err := strconv.Atoi(c.Query("pageSize"))
+	if err != nil {
+		c.JSON(400, map[string]interface{}{
+			"error": "pageSize参数错误",
+		})
+		return
+	}
+	category := c.Query("category")
+
+	products, err := client.ListProducts(ctx, page, pageSize, category)
+	if err != nil {
+		c.JSON(500, map[string]interface{}{
+			"error": "internal server error",
+		})
+		return
+	}
+
+	c.JSON(200, map[string]interface{}{
+		"products": products,
+	})
 }
--- apps/api/infras/client/product.go
+++ apps/api/infras/client/product.go
@@ -1,8 +1,10 @@
 package client
 
 import (
+	"context"
+	"fmt"
+	"github.com/123508/douyinshop/kitex_gen/product"
 	"github.com/123508/douyinshop/kitex_gen/product/productcatalogservice"
-
 	"github.com/123508/douyinshop/pkg/config"
 
 	"time"
@@ -31,4 +33,95 @@ func initProductRpc() {
 		panic(err)
 	}
 	productClient = c
+}
+
+type ProductItem struct {
+	Id           uint32   `json:"id"`
+	Name         string   `json:"name"`
+	Description  string   `json:"description"`
+	Picture      string   `json:"picture"`
+	Price        string   `json:"price"`
+	CategoryName []string `json:"categories"`
+	Sales        int      `json:"sales"`
+}
+
+// ListProducts 获取商品列表
+// page 页码
+// size 每页数量
+// category 分类
+// 返回商品列表
+func ListProducts(ctx context.Context, page int, size int, category string) ([]ProductItem, error) {
+	req := &product.ListProductsReq{
+		Page:         int32(page),
+		PageSize:     int64(size),
+		CategoryName: category,
+	}
+	resp, err := productClient.ListProducts(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	var products []ProductItem
+	for _, p := range resp.Products {
+		products = append(products, ProductItem{
+			Id:           p.Id,
+			Name:         p.Name,
+			Price:        fmt.Sprintf("%.2f", p.Price),
+			Description:  p.Description,
+			CategoryName: p.Categories,
+			Picture:      p.Picture,
+		})
+	}
+	return products, nil
+}
+
+// GetProductDetail 获取商品详情
+// productId 商品ID
+// 返回商品详情
+func GetProductDetail(ctx context.Context, productId int) (*ProductItem, error) {
+	req := &product.GetProductReq{
+		Id: uint32(productId),
+	}
+	resp, err := productClient.GetProduct(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	result := &ProductItem{
+		Id:           resp.Product.Id,
+		Name:         resp.Product.Name,
+		Price:        fmt.Sprintf("%.2f", resp.Product.Price),
+		Description:  resp.Product.Description,
+		CategoryName: resp.Product.Categories,
+		Picture:      resp.Product.Picture,
+		Sales:        int(resp.Product.Sales),
+	}
+	return result, nil
+}
+
+// SearchProducts 搜索商品
+// keyword 关键字
+// page 页码
+// pageSize 每页数量
+// 返回商品列表
+func SearchProducts(ctx context.Context, keyword string, page int, pageSize int) ([]ProductItem, error) {
+	req := &product.SearchProductsReq{
+		Query:    keyword,
+		Page:     uint32(page),
+		PageSize: uint32(pageSize),
+	}
+	resp, err := productClient.SearchProducts(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	var products []ProductItem
+	for _, p := range resp.Results {
+		products = append(products, ProductItem{
+			Id:           p.Id,
+			Name:         p.Name,
+			Price:        fmt.Sprintf("%.2f", p.Price),
+			Description:  p.Description,
+			CategoryName: p.Categories,
+			Picture:      p.Picture,
+		})
+	}
+	return products, nil
 }
