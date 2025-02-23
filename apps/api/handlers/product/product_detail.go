6e0a9ac52a2dd5d51ada70856dcaac8f06042fe6
123508 <47135770+123508@users.noreply.github.com>
2025-02-23 20:18:59 +0800

    feat(apps/api/product): 完成获取商品列表、商品详情和搜索商品功能 (#48)

--- apps/api/handlers/product/product_detail.go
+++ apps/api/handlers/product/product_detail.go
@@ -2,9 +2,31 @@ package product
 
 import (
 	"context"
+	"github.com/123508/douyinshop/apps/api/infras/client"
+	"strconv"
 
 	"github.com/cloudwego/hertz/pkg/app"
 )
 
+// Detail 获取商品详情
 func Detail(ctx context.Context, c *app.RequestContext) {
+	productId, err := strconv.Atoi(c.Param("product_id"))
+	if err != nil {
+		c.JSON(400, map[string]interface{}{
+			"error": "product_id参数错误",
+		})
+		return
+	}
+
+	result, err := client.GetProductDetail(ctx, productId)
+	if err != nil {
+		c.JSON(500, map[string]interface{}{
+			"error": "internal server error",
+		})
+		return
+	}
+
+	c.JSON(200, map[string]interface{}{
+		"product": result,
+	})
 }
