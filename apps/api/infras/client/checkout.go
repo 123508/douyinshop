05bbe92ad4fc437cb9461251db76d212d742481a
wxr <1565858376@qq.com>
2025-02-26 16:43:23 +0800

    修改serviceName

--- apps/api/infras/client/checkout.go
+++ apps/api/infras/client/checkout.go
@@ -1,6 +1,8 @@
 package client
 
 import (
+	"context"
+	"github.com/123508/douyinshop/kitex_gen/checkout"
 	"github.com/123508/douyinshop/kitex_gen/checkout/checkoutservice"
 
 	"github.com/123508/douyinshop/pkg/config"
@@ -31,4 +33,11 @@ func initCheckoutRpc() {
 		panic(err)
 	}
 	checkoutClient = c
+}
+func Checkout(ctx context.Context, req *checkout.CheckoutReq) (bool, error) {
+	_, err := checkoutClient.Checkout(ctx, req)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
 }
