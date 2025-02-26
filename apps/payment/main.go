ba6b0cef00db2db876b4469a51a6d2710fc53b74
shiyuyu <2979724204@qq.com>
2025-02-26 21:05:06 +0800

    feat(apps/api/payment): 完成了支付接口，更正了支付服务的charge函数

--- apps/payment/main.go
+++ apps/payment/main.go
@@ -4,14 +4,37 @@ import (
 	"fmt"
 	payment "github.com/123508/douyinshop/kitex_gen/payment/paymentservice"
 	"github.com/123508/douyinshop/pkg/config"
+	"github.com/123508/douyinshop/pkg/db"
+	"github.com/123508/douyinshop/pkg/models"
 	"github.com/cloudwego/kitex/pkg/rpcinfo"
 	"github.com/cloudwego/kitex/server"
 	etcd "github.com/kitex-contrib/registry-etcd"
+	"gorm.io/gorm"
 	"log"
 	"net"
 )
 
+var database *gorm.DB
+
 func main() {
+	databaseConnection, err := db.InitDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	database = databaseConnection
+	err = database.AutoMigrate(&models.Order{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = database.AutoMigrate(&models.OrderDetail{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = database.AutoMigrate(&models.OrderStatusLog{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	// etcd注册服务
 	r, err := etcd.NewEtcdRegistryWithAuth(config.Conf.EtcdConfig.Endpoints, config.Conf.EtcdConfig.Username, config.Conf.EtcdConfig.Password)
 	if err != nil {
