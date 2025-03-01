314c54557631f15aa13c3898ed6db30ab87ec227
Bloomivy <43897563+Bloomivy@users.noreply.github.com>
2025-03-01 23:21:54 +0800

    feat(apps/orderapps/businessorder apps/api ): 完成了order服务及其http网关实现
    * Order服务开发(userOrder+businessOrder)
    
    * feat(apps/order+apps/businessorder): 添加服务注册代码，完成基本功能，
    
    * feat(apps/api/handlers/order + apps/api/infras/client/order): 完成了order服务模块的http网关实现
    
    * feat(apps/api/handlers/order + apps/api/infras/client/order): 完成了order服务模块的http网关实现

--- apps/api/handlers/order/order_shop_cancel.go
+++ apps/api/handlers/order/order_shop_cancel.go
@@ -2,9 +2,41 @@ package order
 
 import (
 	"context"
+	"github.com/123508/douyinshop/apps/api/infras/client"
+	"strconv"
 
 	"github.com/cloudwego/hertz/pkg/app"
 )
 
 func CancelShop(ctx context.Context, c *app.RequestContext) {
+
+	orderId, err := strconv.Atoi(c.Query("orderId"))
+	if err != nil {
+		c.JSON(400, map[string]interface{}{
+			"error": "orderId 参数错误",
+		})
+		return
+	}
+
+	cancelReason := c.Query("cancelReason")
+	if cancelReason == "" {
+		c.JSON(400, map[string]interface{}{
+			"error": "取消原因不能为空",
+		})
+		return
+	}
+
+	resp, err := client.ShopCancel(ctx, uint32(orderId), cancelReason)
+	if err != nil {
+		c.JSON(500, map[string]interface{}{
+			"error": "订单取消失败",
+		})
+		return
+	}
+
+	c.JSON(200, map[string]interface{}{
+		"message": "订单取消成功",
+		"data":    resp,
+	})
+
 }
--- apps/api/handlers/order/order_shop_rejection.go
+++ apps/api/handlers/order/order_shop_rejection.go
@@ -2,9 +2,41 @@ package order
 
 import (
 	"context"
+	"github.com/123508/douyinshop/apps/api/infras/client"
+	"strconv"
 
 	"github.com/cloudwego/hertz/pkg/app"
 )
 
 func Rejection(ctx context.Context, c *app.RequestContext) {
+
+	orderId, err := strconv.Atoi(c.Query("orderId"))
+	if err != nil {
+		c.JSON(400, map[string]interface{}{
+			"error": "orderId 参数错误",
+		})
+		return
+	}
+
+	rejectionReason := c.Query("rejectionReason")
+	if rejectionReason == "" {
+		c.JSON(400, map[string]interface{}{
+			"error": "拒绝原因不能为空",
+		})
+		return
+	}
+
+	resp, err := client.ShopRejection(ctx, uint32(orderId), rejectionReason)
+	if err != nil {
+		c.JSON(500, map[string]interface{}{
+			"error": "订单拒绝失败",
+		})
+		return
+	}
+
+	c.JSON(200, map[string]interface{}{
+		"message": "订单拒绝成功",
+		"data":    resp,
+	})
+
 }
